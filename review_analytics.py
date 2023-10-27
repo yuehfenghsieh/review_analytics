667ddc55c3e59b47b020a2bc268505223edb29dc
yuehfenghsieh <jason70160@gmail.com>
2023-10-27 23:06:03 +0800

    version01

--- review_analytics.py
+++ review_analytics.py
@@ -5,4 +5,8 @@ with open('reviews.txt', 'r') as f:
 		data.append(line)
 		count += 1
 		if count % 1000 == 0:
-			print(len(data))
+			print('總共有', len(data), '筆留言')
+sum_len = 0
+for d in data:
+	sum_len += len(d)
+print('留言的平均長度是', sum_len / len(data))
